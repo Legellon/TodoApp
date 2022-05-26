b26e84d84657d31ef95ddcf588493a63cae37f84
Nikita Rehemae <nikita.rehemae@gmail.com>
2022-05-26 06:05:57 +0300

    Add delete action in repository and rename _ravenContext to _context

--- RavenTodoApp/Persistence/RavenDbRepository.cs
+++ RavenTodoApp/Persistence/RavenDbRepository.cs
@@ -2,32 +2,41 @@ namespace RavenTodoApp.Persistence;
 
 public class RavenDbRepository<T> : IRepository<T>
 {
-    private readonly IRavenDbContext _ravenContext;
-    
-    public RavenDbRepository(IRavenDbContext ravenContext)
+    private readonly IRavenDbContext _context;
+
+    protected RavenDbRepository(IRavenDbContext context)
     {
-        _ravenContext = ravenContext;
+        _context = context;
     }
     
     public T Get(string id)
     {
-        using var session = _ravenContext.Store.OpenSession();
+        using var session = _context.Store.OpenSession();
         var element = session.Load<T>(id);
         return element;
     }
 
+    public void Delete(string id)
+    {
+        using var session = _context.Store.OpenSession();
+        session.Delete(id);
+        session.SaveChanges();
+    }
+
     public IEnumerable<T> GetAll(int pageSize, int pageNumber)
     {
-        using var session = _ravenContext.Store.OpenSession();
+        using var session = _context.Store.OpenSession();
+        
         var elements = session.Query<T>()
             .Skip(pageSize * (pageNumber - 1))
             .Take(pageSize);
+        
         return elements;
     }
 
     public void InsertOrUpdate(T element)
     {
-        using var session = _ravenContext.Store.OpenSession();
+        using var session = _context.Store.OpenSession();
         session.Store(element);
         session.SaveChanges();
     }
